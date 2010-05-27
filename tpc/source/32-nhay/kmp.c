270b0d5bb7f213141e2b664a0fb3ffcd873beb0a
tutecosta <tutecosta@d1b1875b-4509-22a8-4f88-0435eeb08bb5>
2010-05-27 03:13:42 +0000

    Peinando las mechas.

--- tpc/source/32-nhay/kmp.c
+++ tpc/source/32-nhay/kmp.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #define MAX_HAYSTACK 1024000
 int *pi;
 
 void make_pi(const char *needle, int length) {
 	int i=2, j=0;
 	pi = malloc((length+1) * sizeof(int));
-	pi[0] = -1, pi[1] = 0;
+	pi[0] = -1; pi[1] = 0;
 
-	while (needle[i] != '\0') {
+	while (needle[i] != '\0')
 		if (needle[i-1] == needle[j]) {
 			pi[i] = j+1;
 			i++; j++;
@@ -17,18 +16,16 @@ void make_pi(const char *needle, int length) {
 			j = pi[j];
 		} else {
 			pi[i] = 0;
-			i++;
 			j = 0;
+			i++;
 		}
-	}
 }
 
 int main(int argc, char **argv) {
 	char haystack[MAX_HAYSTACK];
 	int length = 0;
-
 	while (scanf("%i", &length) == 1) {
-		int i=0, m=0, encontro = 0;
+		int i=0, m=0, encontro=0;
 		char needle[length];
 		scanf("%s", needle);
 		scanf("%s", haystack);
@@ -38,10 +35,9 @@ int main(int argc, char **argv) {
 			if (haystack[m + i] == needle[i]) {
 				i++;
 				if (i == length) { /* string found */
-					encontro = 1;
-					printf("%i\n", m);
-					m++;
+					printf("%i\n", m); encontro = 1;
 					i = 0;
+					m++;
 				}
 			} else {
 				m += i - pi[i];
