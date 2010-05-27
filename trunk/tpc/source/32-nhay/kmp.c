80d3ae5e717e87d9d7acda3f0ecb553e1f716dad
tutecosta <tutecosta@d1b1875b-4509-22a8-4f88-0435eeb08bb5>
2010-05-27 02:55:23 +0000

    KMP accepted. :-)

--- trunk/tpc/source/32-nhay/kmp.c
+++ trunk/tpc/source/32-nhay/kmp.c
@@ -2,55 +2,51 @@
 #include<stdlib.h>
 #include<string.h>
 #define MAX_HAYSTACK 1024000
+int *pi;
 
-int *f; /* Failure function */
+void make_pi(const char *needle, int length) {
+	int i=2, j=0;
+	pi = malloc((length+1) * sizeof(int));
+	pi[0] = -1, pi[1] = 0;
 
-int make_f(const char *needle) {
-	int t = 0; /* think of t+1 as the length of the current valid prefix. */
-	int s,m;
-
-	m = strlen(needle);
-	f = malloc((m+1)*sizeof(int));
-
-	f[1] = 0; /* When first character is received there is no proper suffix. */
-
-	for (s=1; s < m; s++) { /* Create f[s] */
-		while ((t > 0) && (needle[s] != needle[t]))
-			t = f[t];
-		if (needle[t] == needle[s]) {
-			/* Next character can be added to current prefix */
-			t++;
-			f[s+1] = t;
-		} else
-			f[s+1] = 0; /* while loop must have exited (t=0) */
+	while (needle[i] != '\0') {
+		if (needle[i-1] == needle[j]) {
+			pi[i] = j+1;
+			i++; j++;
+		} else if (j > 0) {
+			j = pi[j];
+		} else {
+			pi[i] = 0;
+			i++;
+			j = 0;
+		}
 	}
 }
 
 int main(int argc, char **argv) {
-	char haystack[MAX_HAYSTACK],*p;
-	int c,i,length = 0;
-	int lines = 0;
-	char *ll;
+	char haystack[MAX_HAYSTACK];
+	int length = 0;
 
 	while (scanf("%i", &length) == 1) {
-		int encontro = 0;
+		int i=0, m=0, encontro = 0;
 		char needle[length];
 		scanf("%s", needle);
 		scanf("%s", haystack);
 
-		make_f(needle);
-		p = haystack;
-		while (p = strstr(p,needle)) {
-			encontro = 1;
-			p++;
-			ll = haystack; /* Position of start of most recent line */
-			for (i=0; i < p-haystack; i++) {
-				if (haystack[i] == '\n') {
-					ll = haystack + i + 1;
-					lines++;
+		make_pi(needle, length);
+		while (haystack[m + i] != '\0' && needle[i] != '\0') {
+			if (haystack[m + i] == needle[i]) {
+				i++;
+				if (i == length) { /* string found */
+					encontro = 1;
+					printf("%i\n", m);
+					m++;
+					i = 0;
 				}
+			} else {
+				m += i - pi[i];
+				if (i > 0) i = pi[i];
 			}
-			printf("%d\n", p-ll-1);
 		}
 		if(!encontro) putchar('\n');
 	}
