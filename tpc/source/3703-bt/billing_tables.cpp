52a1026d9472adea6fedd8f853f66b98a45a6585
hadeskleizer <hadeskleizer@d1b1875b-4509-22a8-4f88-0435eeb08bb5>
2010-06-02 23:20:10 +0000

    Billing Tables: Arreglamos el tema de los ceros al principio, pero sigue sin pasar.

--- tpc/source/3703-bt/billing_tables.cpp
+++ tpc/source/3703-bt/billing_tables.cpp
@@ -1,27 +1,28 @@
 //#define FILEINPUT
 #ifdef FILEINPUT
 #include <fstream>
-#else
-#include <iostream>
 #endif
 
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#define forn(i, n) for(int i = 0; i < (int)(n); ++i)
 
 using namespace std;
 
 #define MAXHIJOSNODO 10
+#define MAXLONGPREFIJO 11
 
 struct nodo {
-    // prefijo
+	// prefijo
 	char *s;
 
-    // bill plan
-    char *b;
+	// bill plan
+	char *b;
 
-    // punteros a hijos
+	// punteros a hijos
 	nodo *hijos[ MAXHIJOSNODO ];
 };
 
@@ -35,11 +36,11 @@ inline int ord( char c ) {
 
 inline long long int pow10( int i )
 {
-    long long int res = 1;
-    while( i-- > 0 )
-        res *= 10;
+	long long int res = 1;
+	while( i-- > 0 )
+		res *= 10;
 
-    return res;
+	return res;
 }
 
 void agregar( char *t, char *b ) {
@@ -47,18 +48,18 @@ void agregar( char *t, char *b ) {
 	char *p;
 
 	previo = &raiz;
-    actual = previo->hijos[ ord(*t) ];
+	actual = previo->hijos[ ord(*t) ];
 
-    while( actual && *t ) {
+	while( actual && *t ) {
 		for( p = actual->s; *t == *p && *t; t++,p++ );
 
-        // Si el prefijo que quiero agregar tiene
-        // como prefijo a otro que ya agregue
-        if( actual->b && !*p )
-            return;
+		// Si el prefijo que quiero agregar tiene
+		// como prefijo a otro que ya agregue
+		if( actual->b && !*p )
+			return;
 
-        // Si tienen prefijo en comun pero ninguno de
-        // los dos es prefijo del otro parto el nodo
+		// Si tienen prefijo en comun pero ninguno de
+		// los dos es prefijo del otro parto el nodo
 		if( *p ) {
 			nodo *nuevo = new nodo;
 			nuevo->s = new char[ strlen(p) + 1 ];
@@ -67,82 +68,78 @@ void agregar( char *t, char *b ) {
 				nuevo->hijos[i] = actual->hijos[i];
 				actual->hijos[i] = NULL;
 			}
-            nuevo->b = actual->b;
-            actual->b = NULL;
+			nuevo->b = actual->b;
+			actual->b = NULL;
 			actual->hijos[ ord(*p) ] = nuevo;
 			*p = '\0';
 		}
 
 		previo = actual;
-        if( *t )
-            actual = previo->hijos[ ord(*t) ];
+		if( *t )
+			actual = previo->hijos[ ord(*t) ];
 	}
 
-    if( !*t ) {
-        /*
-         * Si viene un numero que es prefijo de los demas
-         * tengo que extender el arbol en todos los lugares
-         * donde se pueden agregar hojas
-         */
-        int top = 0;
-        nodo *pila[ 10*11 ];
-        pila[ top ] = previo;
-        while( top >= 0 )
-        {
-            actual = pila[ top ];
-            top--;
-            if( !actual->b || ( actual->s && strlen(actual->s) > 1 ) )
-            {
-                if( actual->s && strlen(actual->s) > 1 )
-                {
-                    // parto el nodo
-			        nodo *nuevo = new nodo;
-			        nuevo->s = new char[ strlen(actual->s + 1) + 1 ];
-                    strcpy( nuevo->s, actual->s + 1 );
-			        for( int i = 0; i < MAXHIJOSNODO; i++ ) {
-				        nuevo->hijos[i] = actual->hijos[i];
-				        actual->hijos[i] = NULL;
-			        }
-                    nuevo->b = actual->b;
-                    actual->b = NULL;
-                    actual->s[1] = '\0';
-			        actual->hijos[ ord( *nuevo->s ) ] = nuevo;
-                }
+	if( !*t ) {
+		/*
+		 * Si viene un numero que es prefijo de los demas
+		 * tengo que extender el arbol en todos los lugares
+		 * donde se pueden agregar hojas
+		 */
+		int top = 0;
+		nodo *pila[ MAXHIJOSNODO*MAXLONGPREFIJO ];
+		pila[ top ] = previo;
+		while( top >= 0 )
+		{
+			actual = pila[ top ];
+			top--;
+			if( !actual->b || ( actual->s && strlen(actual->s) > 1 ) ) {
+				if( actual->s && strlen(actual->s) > 1 ) {
+					// parto el nodo
+					nodo *nuevo = new nodo;
+					nuevo->s = new char[ strlen(actual->s + 1) + 1 ];
+					strcpy( nuevo->s, actual->s + 1 );
+					for( int i = 0; i < MAXHIJOSNODO; i++ ) {
+						nuevo->hijos[i] = actual->hijos[i];
+						actual->hijos[i] = NULL;
+					}
+					nuevo->b = actual->b;
+					actual->b = NULL;
+					actual->s[1] = '\0';
+					actual->hijos[ ord( *nuevo->s ) ] = nuevo;
+				}
 
-                for( int i = MAXHIJOSNODO - 1; i >= 0; i-- )
-                {
-                    if( actual->hijos[i] )
-                        pila[ ++top ] = actual->hijos[i];
-                    else
-                    {
-			            nodo *nuevo = new nodo;
-			            nuevo->s = new char[ 2 ];
-                        sprintf( nuevo->s, "%d", i );
-                        nuevo->b = new char[ strlen(b) + 1 ];
-                        strcpy( nuevo->b, b );
-			            for( int j = 0; j < MAXHIJOSNODO; j++ )
-				            nuevo->hijos[j] = NULL;
-                        actual->hijos[i] = nuevo;
-                        if( strcmp( b, "invalid" ) != 0 )
-                            cardinal++;
-                    }
-                }
-            }
-        }
-    } else {
-        if( !actual ) {
-            nodo *nuevo = new nodo;
-	        nuevo->s = new char[ strlen(t) + 1 ];
-	        nuevo->b = new char[ strlen(b) + 1 ];
-	        strcpy( nuevo->s, t );
-            strcpy( nuevo->b, b );
-	        for( int i = 0; i < MAXHIJOSNODO; i++ )
-		        nuevo->hijos[i] = NULL;
-            previo->hijos[ ord(*t) ] = nuevo;
-            if( strcmp( b, "invalid" ) != 0 )
-                cardinal++;
-        }
-    }
+				for( int i = MAXHIJOSNODO - 1; i >= 0; i-- ) {
+					if( actual->hijos[i] )
+						pila[ ++top ] = actual->hijos[i];
+					else {
+						nodo *nuevo = new nodo;
+						nuevo->s = new char[ 2 ];
+						sprintf( nuevo->s, "%d", i );
+						nuevo->b = new char[ strlen(b) + 1 ];
+						strcpy( nuevo->b, b );
+						for( int j = 0; j < MAXHIJOSNODO; j++ )
+							nuevo->hijos[j] = NULL;
+						actual->hijos[i] = nuevo;
+						if( strcmp( b, "invalid" ) != 0 )
+							cardinal++;
+					}
+				}
+			}
+		}
+	} else {
+		if( !actual ) {
+			nodo *nuevo = new nodo;
+			nuevo->s = new char[ strlen(t) + 1 ];
+			nuevo->b = new char[ strlen(b) + 1 ];
+			strcpy( nuevo->s, t );
+			strcpy( nuevo->b, b );
+			for( int i = 0; i < MAXHIJOSNODO; i++ )
+				nuevo->hijos[i] = NULL;
+			previo->hijos[ ord(*t) ] = nuevo;
+			if( strcmp( b, "invalid" ) != 0 )
+				cardinal++;
+		}
+	}
 } 
 
 void destruir( nodo *padre ) {
@@ -156,7 +153,7 @@ void destruir( nodo *padre ) {
 	}
 	if( padre != &raiz ) {
 		delete[] padre->s;
-        delete[] padre->b;
+		delete[] padre->b;
 		delete padre;
 	}
 }
@@ -166,108 +163,111 @@ void generarTest( const char* nombreArchivo ) {
 
 inline int digitos( long long int n )
 {
-    int d;
-    
-    if( n == 0 )
-        return 1;
+	int d;
+	
+	if( n == 0 )
+		return 1;
 
-    for( d = 0; n > 0; d++ )
-        n /= 10;
-    return d;
+	for( d = 0; n > 0; d++ )
+		n /= 10;
+	return d;
 }
 
 inline long long int primerosNDigitos( int n, long long int numero )
 {
-    return numero % ( pow10(n) );
+	return numero % ( pow10(n) );
 }
 
 void printAux( nodo* t, char* p )
 {
 	int i;
-    char conc[11];
+	char conc[MAXLONGPREFIJO + 1];
 
-    if(t && ( !t->b || strcmp( t->b, "invalid" ) != 0 ) ) {
-        if( t->b )
-            cout << p << t->s << " " << t->b << endl;
-        else {
-            for( i = 0; i < MAXHIJOSNODO; i++ ) {
-                if( p ) {
-                    strcpy( conc, p );
-                    strcat( conc, t->s );
-                    printAux( t->hijos[i], conc );
-                }
-                else {
-                    printAux( t->hijos[i], p );
-                }
-            }
-        }
-    }
+	if(t && ( !t->b || strcmp( t->b, "invalid" ) != 0 ) ) {
+		if( t->b )
+			cout << p << t->s << " " << t->b << endl;
+		else {
+			for( i = 0; i < MAXHIJOSNODO; i++ ) {
+				if( p ) {
+					strcpy( conc, p );
+					strcat( conc, t->s );
+					printAux( t->hijos[i], conc );
+				}
+				else {
+					printAux( t->hijos[i], p );
+				}
+			}
+		}
+	}
 }
 
 void print( nodo* t )
 {
-    int i;
-    for( i = 0; i < MAXHIJOSNODO; i++ )
-        printAux( raiz.hijos[i], "" );
+	int i;
+	for( i = 0; i < MAXHIJOSNODO; i++ )
+		printAux( raiz.hijos[i], "" );
 }
 
 int main() {
 #ifdef FILEINPUT
-    generarTest("test");
-    ifstream entrada( "test", ios_base::in );
+//	generarTest("test");
+	ifstream entrada( "input_mati", ios_base::in );
 #else
-    istream& entrada = cin;
+	istream& entrada = cin;
 #endif
 
-    int n, i;
-    long long int pi, pj, dif;
-    char p[11], bp[1024];
+	int n, i, ceros, numTest=0;
+	long long int pi, pj, dif;
+	char p[MAXLONGPREFIJO + 1], bp[1024];
 
-    while( entrada >> n )
-    {
-        cardinal = 0;
-        while( n-- > 0 )
-        {
-            entrada.ignore(); // ignoramos el '\n'
-            entrada >> pi;
-            entrada.ignore();
-            entrada.ignore();
-            entrada >> pj;
-            entrada >> bp;
+	while( entrada >> n ) {
+		if (numTest++ > 0)
+			cout << endl;
 
-            pj = pi - primerosNDigitos( digitos(pj), pi ) + pj;
-            dif = pj - pi + 1;
-            i = 0;
-            while( dif > 0 ) {
-                while( !( pi % 10 > 0 ) && dif >= pow10(i) )
-                {
-                    i++;
-                    pi /= 10;
-                }
+		cardinal = 0;
+		while( n-- > 0 ){
+			entrada.ignore(); // ignoramos el '\n'
+			entrada >> p;
 
-                while( dif < pow10(i) )
-                {
-                    i--;
-                    pi *= 10;
-                }
+			pi = atoi(p);
+			int di = strlen(p);
 
-                sprintf( p, "%lld", pi );
-                agregar( p, bp );
-                dif -= pow(10.0f, i);
-                pi++;
-            }
-        }
-        cout << cardinal << endl;
+			entrada.ignore(); /* Guion y espacio */
+			entrada.ignore();
+			entrada >> pj;
+			entrada >> bp;
 
-        /* Imprimo el arbol por el stdout */
-        print( &raiz );
-        cout << endl;
+			pj = pi - primerosNDigitos( digitos(pj), pi ) + pj;
+			dif = pj - pi + 1;
+			i = 0;
+			while( dif > 0 ) {
+				while( !( pi % 10 > 0 ) && dif >= pow10(i) ) {
+					i++;
+					pi /= 10;
+				}
 
-        destruir( &raiz );
-    }
+				while( dif < pow10(i) ) {
+					i--;
+					pi *= 10;
+				}
+
+				int cerosNuevo = di - digitos(pi) - i;
+				sprintf( p + cerosNuevo, "%lld", pi );
+				agregar( p, bp );
+				dif -= pow(10.0f, i);
+				pi++;
+			}
+		}
+		cout << cardinal << endl;
+
+		/* Imprime árbol por stdout */
+		print( &raiz );
+
+		destruir( &raiz );
+	}
 
 #ifdef FILEINPUT
-    entrada.close();
+	entrada.close();
 #endif
-    return 0;
+	return 0;
 }
