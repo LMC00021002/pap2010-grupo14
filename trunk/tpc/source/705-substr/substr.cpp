b2783b22e8e5d3077c7a2d1ead689267f1bbdc91
hadeskleizer <hadeskleizer@d1b1875b-4509-22a8-4f88-0435eeb08bb5>
2010-06-17 07:24:16 +0000

    Complete un poco mas el informe de substr y cambie boludeces del codigo.

--- trunk/tpc/source/705-substr/substr.cpp
+++ trunk/tpc/source/705-substr/substr.cpp
@@ -7,7 +7,7 @@
 #define MAXN 50000
 
 int N, delta;
-char string[MAXN + 1];
+char cadena[MAXN + 1];
 int suffixArray[MAXN + 1];
 int rank[MAXN + 1];
 int tmp[MAXN + 1];
@@ -34,28 +34,32 @@ int main() {
     scanf( "%d\n", &T );
     while( T-- > 0 )
     {
-	    scanf( "%s", &string );
-	    N = strlen( string );
+	    scanf( "%s", &cadena );
+	    N = strlen( cadena );
 
 	    forn( i, N )
         {
 		    suffixArray[i] = i;
-		    rank[i] = string[i];
+		    rank[i] = cadena[i];
 	    }
 
 	    for ( delta = 1; delta == 1 || tmp[N - 1] != N - 1; delta *= 2 )
         {
             std::sort( suffixArray, suffixArray + N, comp );
-		    forn( i, N - 1 ) tmp[i + 1] = tmp[i] + comp( suffixArray[i], suffixArray[i + 1] );
-		    forn( i, N ) rank[ suffixArray[i] ] = tmp[i];
+		    forn( i, N - 1 )
+                tmp[i + 1] = tmp[i] + comp( suffixArray[i], suffixArray[i + 1] );
+		    forn( i, N )
+                rank[ suffixArray[i] ] = tmp[i];
 	    }
 
 	    for ( int i = 0, k = 0; i < N; i++ ) {
-		    if ( rank[i] == N - 1 )
-			    continue;
-		    for ( int j = suffixArray[ rank[i] + 1 ]; string[i + k] == string[j + k]; k++ );
-		    LCP[ rank[i] ] = k;
-		    if ( k > 0 ) k--;
+		    if ( rank[i] != N - 1 )
+            {
+		        for ( int j = suffixArray[ rank[i] + 1 ]; cadena[i + k] == cadena[j + k]; k++ );
+		            LCP[ rank[i] ] = k;
+		        if ( k > 0 )
+                    k--;
+            }
 	    }
 
         printf( "%lld\n", ((long long) N)*((long long) N+1)/2 - sumatoria(LCP, N) );
