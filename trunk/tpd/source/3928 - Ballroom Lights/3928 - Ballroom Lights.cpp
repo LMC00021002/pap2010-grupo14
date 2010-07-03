1eaf77867857356e3c9de15166162d597c90660f
hadeskleizer <hadeskleizer@d1b1875b-4509-22a8-4f88-0435eeb08bb5>
2010-07-03 01:11:48 +0000

    3928 - Ballroom Lights: complejidad mejorada.

--- trunk/tpd/source/3928 - Ballroom Lights/3928 - Ballroom Lights.cpp
+++ trunk/tpd/source/3928 - Ballroom Lights/3928 - Ballroom Lights.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <vector>
-#include <list>
+#include <algorithm>
 
 #ifdef _DEBUG
 #include <fstream>
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-#define forn( i, n ) for( int i = 0; i < n; i++ )
+#define forn( i, n ) for( int i = 0; i < (n); i++ )
 #define EPSILON 0.0000001l
 
 inline double min( double a, double b ) { return a < b ? a : b; }
@@ -26,7 +26,7 @@ public:
     Par( double _x, double _y ) : x( _x ), y( _y ) {};
     Par( const Par& p ) : x( p.x ), y( p.y ) {};
     Par operator-( const Par& p ) const { return Par( x - p.x, y - p.y ); }
-    bool operator<( const Par& p ) const { return y + EPSILON < p.x; }
+    bool operator<( const Par& p ) const { return x < p.x || ( fabs(x - p.x) < EPSILON && y < p.y ); }
     bool operator==( const Par& p ) const { return fabs(x - p.x) < EPSILON && fabs(y - p.y) < EPSILON; }
     bool operator!=( const Par& p ) const { return fabs(x - p.x) > EPSILON || fabs(y - p.y) > EPSILON; }
 
@@ -246,62 +246,53 @@ inline Par unionIntervalosND( const Par& p1, const Par& p2 )
     return Par( min( p1.x, p2.x ), max( p1.y, p2.y ) );
 }
 
-inline void unir( list< Par >& conj, const Par& par, double minVal, double maxVal )
+void comprimir( vector< Par >& pares )
 {
-    // Si el intervalo "pega la vuelta", lo dividimos
-    Par p1 = par;
-    Par p2 = par;
-    if( par.x > par.y ) {
-        p1 = Par( par.x, maxVal );
-        p2 = Par( minVal, par.y );
-    }
+    int n = pares.size();
+    if( n > 0 ) {
+        vector< Par > res;
 
-    list< Par >::iterator it;
-    for( it = conj.begin(); it != conj.end() && (*it) < p1; it++ ){}
+        sort( pares.begin(), pares.end() );
 
-    if( it == conj.end() || it->x > p1.y )
-        conj.insert( it, p1 );
-    else {
-        *it = unionIntervalosND( p1, *it );
-        Par* p = &*it;
-        it++;
+        res.push_back( pares[0] );
+        int ultimo = 0;
 
-        while( it != conj.end() && it->x <= p->y ) {
-            if( it->y > p->y )
-                p->y = it->y;
-            it = conj.erase( it );
+        for( int i = 1; i < n; i++ ) {
+            if( res[ ultimo ].y >= pares[ i ].x ) {
+                if( res[ ultimo ].y < pares[ i ].y )
+                    res[ ultimo ].y = pares[ i ].y;
+            }
+            else {
+                res.push_back( pares[ i ] );
+                ultimo++;
+            }
         }
-    }
 
-    if( p1 != p2 )
-        unir( conj, p2, minVal, maxVal );
+        pares = res;
+    }
 }
 
-void complemento( list< Par >& conj, double minVal, double maxVal )
+void complemento( vector< Par >& conj, double minVal, double maxVal )
 {
+    comprimir( conj );
+
     int n = conj.size();
-    double xInicial = conj.front().x;
-    double yFinal = conj.back().y;
+    vector< Par > res;
 
     if( n == 0 )
-        conj.push_back( Par( minVal, maxVal ) );
+        res.push_back( Par( minVal, maxVal ) );
     else {
-        list< Par >::iterator actual = conj.begin();
-        list< Par >::iterator anterior = actual;
-        actual++;
-        for( int i = 0; i < n - 1; actual++, anterior++, i++ )
-            conj.push_back( Par( anterior->y, actual->x ) );
+        if( fabs(minVal - conj[0].x) > EPSILON )
+            res.push_back( Par( minVal, conj[0].x ) );
+
+        for( int i = 0; i < n - 1; i++ )
+            res.push_back( Par( conj[i].y, conj[i + 1].x ) );
+
+        if( fabs( conj[n-1].y - maxVal ) > EPSILON )
+            res.push_back( Par( conj[n-1].y, maxVal ) );
     }
 
-    forn( i, n )
-        conj.pop_front();
-
-    if( fabs(xInicial - minVal) > EPSILON )
-        conj.push_back( Par( yFinal, xInicial ) );
-    else {
-        if( fabs(yFinal - maxVal) > EPSILON )
-            conj.push_back( Par( yFinal, xInicial ) );
-    }
+    conj = res;
 }
 
 double perimIluminado()
@@ -314,23 +305,34 @@ double perimIluminado()
 	if( cantColumnas == 0 )
 		return maxVal;
 
-    list< Par > iluminado;
+    vector< Par > iluminado;
 
-    for( vector< Luz >::const_iterator l = luces.begin(); l < luces.end(); l++ ) {
-        list< Par > intervalos;
+    forn( i, cantLuces ) {
+        vector< Par > intervalos;
 
-        for( vector< Columna >::const_iterator c = columnas.begin(); c < columnas.end(); c++ )
-            unir( intervalos, intervaloSombra( *l, *c ), minVal, maxVal );
+        forn( j, cantColumnas ) {
+            Par sombra = intervaloSombra( luces[i], columnas[j] );
+            if( sombra.x > sombra.y ) {
+                intervalos.push_back( Par( minVal, sombra.y ) );
+                intervalos.push_back( Par( sombra.x, maxVal ) );
+            }
+            else
+                intervalos.push_back( sombra );
+        }
         
         complemento( intervalos, minVal, maxVal );
         
-        for( list< Par >::iterator it = intervalos.begin(); it != intervalos.end(); it++ )
-            unir( iluminado, *it, minVal, maxVal );
+        int sizeIntervalos = intervalos.size();
+        forn( i, sizeIntervalos )
+            iluminado.push_back( intervalos[i] );
     }
 
+    comprimir( iluminado );
+
+    int sizeIluminado = iluminado.size();
     double res = 0;
-    for( list< Par >::iterator it = iluminado.begin(); it != iluminado.end(); it++ )
-        res += it->y - it->x;
+    forn( i, sizeIluminado )
+        res += iluminado[i].y - iluminado[i].x;
 
     return res;
 }
@@ -658,30 +660,24 @@ bool testIntervaloSombra()
 
     return paso;
 }
-bool testUnir()
+
+bool testComprimir()
 {
     bool paso = true;
-    list< Par > l;
-    int minVal = 0;
-    int maxVal = (ancho << 1) + (alto << 1);
+    vector< Par > v;
 
-    unir( l, Par( 0, 1 ), minVal, maxVal );
-    unir( l, Par( 3, 4 ), minVal, maxVal );
-    unir( l, Par( 5, 6 ), minVal, maxVal );
-    unir( l, Par( 0, 3 ), minVal, maxVal );
-    unir( l, Par( 6, 10 ), minVal, maxVal );
-    unir( l, Par( 6, 7 ), minVal, maxVal );
-    unir( l, Par( 15, 16 ), minVal, maxVal );
-    unir( l, Par( 17, 20 ), minVal, maxVal );
-    unir( l, Par( 4, 17 ), minVal, maxVal );
+    v.push_back( Par( 0, 1 ) );
+    v.push_back( Par( 3, 4 ) );
+    v.push_back( Par( 5, 6 ) );
+    v.push_back( Par( 0, 3 ) );
+    v.push_back( Par( 6, 10 ) );
+    v.push_back( Par( 6, 7 ) );
+    v.push_back( Par( 15, 16 ) );
+    v.push_back( Par( 17, 20 ) );
+    v.push_back( Par( 4, 17 ) );
 
-    paso = paso && l.size() == 1 && l.front() == Par( 0, 20 );
-
-    l.clear();
-    unir( l, Par( 6, 26 ), minVal, maxVal );
-    unir( l, Par( 14, 2 ), minVal, maxVal );
-
-    paso = paso && l.size() == 2 && l.front() == Par( 0, 2 ) && l.back() == Par( 6, maxVal );
+    comprimir( v );
+    paso = paso && v.size() == 1 && v.front() == Par( 0, 20 );
 
     return paso;
 }
@@ -689,23 +685,23 @@ bool testUnir()
 bool testComplemento()
 {
     bool paso = true;
-    list< Par > l;
-    list< Par > resultado;
+    vector< Par > v;
+    vector< Par > resultado;
 
-    l.push_back( Par( 0, 1 ) );
-    l.push_back( Par( 2, 3 ) );
-    l.push_back( Par( 4, 5 ) );
-    l.push_back( Par( 6, 7 ) );
-    l.push_back( Par( 8, 9 ) );
-    complemento( l, 0, 10 );
+    v.push_back( Par( 0, 1 ) );
+    v.push_back( Par( 2, 3 ) );
+    v.push_back( Par( 4, 5 ) );
+    v.push_back( Par( 6, 7 ) );
+    v.push_back( Par( 8, 9 ) );
+    complemento( v, 0, 10 );
 
     resultado.push_back( Par( 1, 2 ) );
     resultado.push_back( Par( 3, 4 ) );
     resultado.push_back( Par( 5, 6 ) );
     resultado.push_back( Par( 7, 8 ) );
-    resultado.push_back( Par( 9, 0 ) );
+    resultado.push_back( Par( 9, 10 ) );
 
-    paso = paso && l == resultado;
+    paso = paso && v == resultado;
 
     resultado.clear();
 
@@ -719,11 +715,11 @@ int main()
     assert( testInterSemirrectas() );
     assert( testInterSemirrectaSeg() );
     assert( testIntervaloSombra() );
-    assert( testUnir() );
+    assert( testComprimir() );
     assert( testComplemento() );
 
-    ifstream entrada( "test2.txt", ios_base::in );
-    ofstream salida( "test2.out", ios_base::out );
+    ifstream entrada( "test.in", ios_base::in );
+    ofstream salida( "test.out", ios_base::out );
 #endif
 
     while( true ) {
