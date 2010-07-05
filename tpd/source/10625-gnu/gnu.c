565d1da49c30ba85215134294ef0013c62605f66
tutecosta <tutecosta@d1b1875b-4509-22a8-4f88-0435eeb08bb5>
2010-07-05 07:59:37 +0000

    WA en GNU. Cerquiita! :-s

--- tpd/source/10625-gnu/gnu.c
+++ tpd/source/10625-gnu/gnu.c
@@ -1,21 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define forn(i, n) for(i=0; i < (int)(n); ++i)
+#define max(a,b) ((a)>(b) ? (a) : (b))
 #define MAX_RULE 94
+#define OFFSET 33
 
 int orig[MAX_RULE][MAX_RULE];
 int aux[MAX_RULE][MAX_RULE];
-
-void show_matrix(int (*a)[MAX_RULE]) {
-	int i, j;
-	forn(i, MAX_RULE) {
-		printf("%c (%i)\t", i+33, i+33);
-		forn(j, MAX_RULE)
-			printf("%d ", a[i][j]);
-		printf("\n");
-	}
-	printf("\n");
-}
+int aux2[MAX_RULE][MAX_RULE];
 
 int num_chars(const unsigned char *str, char c) {
 	int i=0, count=0;
@@ -37,21 +29,19 @@ void mult_matrix(int (*a)[MAX_RULE], int (*b)[MAX_RULE]) {
 	forn(i, MAX_RULE)
 		forn(j, MAX_RULE)
 			res[i][j] = 0;
-
 	forn(i, MAX_RULE)
 		forn(j, MAX_RULE)
 			forn(k, MAX_RULE)
 				res[i][j] += a[i][k] * b[k][j];
-
 	cp_matrix(a, res);
 }
 
 void matrix_exp(int exp) {
-	if (exp >= 2) {
-		mult_matrix(aux, aux); /* aux^2 */
-		matrix_exp(exp >> 1);  /* exp/2 */
-		if (exp & 1)
-			mult_matrix(aux, orig);
+	for(;;) {
+		if (exp % 2 == 1)
+			mult_matrix(aux, aux2);
+		if ((exp /= 2) == 0) break;
+		mult_matrix(aux2, aux2);
 	}
 }
 
@@ -60,7 +50,7 @@ int main(int argc, char **argv) {
 	unsigned char rule[MAX_RULE], str[MAX_RULE], c;
 	int num_cases;
 	scanf("%i", &num_cases);
-	while(num_cases--) {
+	while (num_cases--) {
 		forn(i, MAX_RULE)
 			forn(j, MAX_RULE)
 				orig[i][j] = 0; /* Reset orig */
@@ -68,18 +58,24 @@ int main(int argc, char **argv) {
 		scanf("%i", &rules);
 		while (rules--) {
 			scanf("%s", rule);
-			for(i=3; rule[i] != 0; i++)
-				orig[rule[0]-33][rule[i]-33] += 1;
+			for(i=3; rule[i] != 0; i++) {
+				orig[rule[0]-OFFSET][rule[i]-OFFSET]++;
+				orig[rule[i]-OFFSET][rule[i]-OFFSET] = 1;
+			}
 		}
 
 		scanf("%i", &queries);
 		while (queries--) {
 			res = 0;
 			scanf("%s %c %i", str, &c, &its);
-			cp_matrix(aux, orig); /* Reset aux */
-			matrix_exp(its);      /* aux matrix exponenciada */
+			/* Reset aux & aux2 */
 			forn(i, MAX_RULE)
-				res += aux[i][c-33];
+				forn(j, MAX_RULE)
+					aux[i][j] = (i==j);
+			cp_matrix(aux2, orig);
+			matrix_exp(its); /* aux <- matriz exponenciada */
+			forn(i, MAX_RULE)
+				res += aux[i][c-OFFSET] * max(num_chars(str, i+OFFSET), 1);
 			printf("%i\n", res);
 		}
 	}
