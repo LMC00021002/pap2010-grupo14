5e65aacdb5852e54d9cdc68f2f9ebbdeef4acfea
tutecosta <tutecosta@d1b1875b-4509-22a8-4f88-0435eeb08bb5>
2010-07-06 05:04:22 +0000

    GNU accepted!

--- trunk/tpd/source/10625-gnu/gnu.cpp
+++ trunk/tpd/source/10625-gnu/gnu.cpp
@@ -1,24 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<iostream>
+using namespace std;
+
 #define forn(i, n) for(i=0; i < (int)(n); ++i)
 #define max(a,b) ((a)>(b) ? (a) : (b))
 #define MAX_RULE 94
 #define OFFSET 33
 
-int orig[MAX_RULE][MAX_RULE];
-int aux[MAX_RULE][MAX_RULE];
-int aux2[MAX_RULE][MAX_RULE];
-
-void show_matrix(int (*a)[MAX_RULE]) {
-	int i, j;
-	forn(i, MAX_RULE) {
-		printf("%c (%i)\t", i+OFFSET, i+OFFSET);
-		forn(j, MAX_RULE)
-			printf("%d", a[i][j]);
-		printf("\n");
-	}
-	printf("\n");
-}
+unsigned long long int orig[MAX_RULE][MAX_RULE];
+unsigned long long int aux[MAX_RULE][MAX_RULE];
+unsigned long long int aux2[MAX_RULE][MAX_RULE];
+int in_queries[MAX_RULE];
 
 int num_chars(const unsigned char *str, char c) {
 	int i=0, count=0;
@@ -27,27 +20,31 @@ int num_chars(const unsigned char *str, char c) {
 	return count;
 }
 
-void cp_matrix(int (*a)[MAX_RULE], int (*b)[MAX_RULE]) {
+void cp_matrix(unsigned long long int (*a)[MAX_RULE], unsigned long long int (*b)[MAX_RULE]) {
 	int i,j;
 	forn(i, MAX_RULE)
 		forn(j, MAX_RULE)
 			a[i][j] = b[i][j];
 }
 
-void mult_matrix(int (*a)[MAX_RULE], int (*b)[MAX_RULE]) {
+void mult_matrix(unsigned long long int (*a)[MAX_RULE], unsigned long long int (*b)[MAX_RULE]) {
 	/* Sobreescribe a */
-	int i, j, k, res[MAX_RULE][MAX_RULE];
+	int i, j, k;
+	unsigned long long int res[MAX_RULE][MAX_RULE];
 	forn(i, MAX_RULE)
 		forn(j, MAX_RULE)
 			res[i][j] = 0;
 	forn(i, MAX_RULE)
-		forn(j, MAX_RULE)
+		forn(j, MAX_RULE) {
+			if (in_queries[i] && !in_queries[j])
+				res[i][j] += a[i][j];
 			forn(k, MAX_RULE)
 				res[i][j] += a[i][k] * b[k][j];
+		}
 	cp_matrix(a, res);
 }
 
-void matrix_exp(int exp) {
+void matrix_exp(unsigned long long int exp) {
 	for(;;) {
 		if (exp % 2 == 1)
 			mult_matrix(aux, aux2);
@@ -57,22 +54,24 @@ void matrix_exp(int exp) {
 }
 
 int main(int argc, char **argv) {
-	int i, j, rules, queries, its, res;
+	int i, j, rules, queries, its;
+	unsigned long long int res;
 	unsigned char rule[MAX_RULE], str[MAX_RULE], c;
 	int num_cases;
 	scanf("%i", &num_cases);
 	while (num_cases--) {
-		forn(i, MAX_RULE)
+		forn(i, MAX_RULE) {
+			in_queries[i] = 0;  /* in_queries vacío */
 			forn(j, MAX_RULE)
 				orig[i][j] = 0; /* Reset orig */
+		}
 
 		scanf("%i", &rules);
 		while (rules--) {
 			scanf("%s", rule);
-			for(i=3; rule[i] != 0; i++) {
+			in_queries[rule[0]-OFFSET] = 1;
+			for(i=3; rule[i] != 0; i++)
 				orig[rule[0]-OFFSET][rule[i]-OFFSET]++;
-				orig[rule[i]-OFFSET][rule[i]-OFFSET] = 1;
-			}
 		}
 
 		scanf("%i", &queries);
@@ -86,8 +85,9 @@ int main(int argc, char **argv) {
 			cp_matrix(aux2, orig);
 			matrix_exp(its); /* aux <- matriz exponenciada */
 			forn(i, MAX_RULE)
-				res += aux[i][c-OFFSET] * max(num_chars(str, i+OFFSET), 1);
-			printf("%i\n", res);
+				res += aux[i][c-OFFSET] * num_chars(str, i+OFFSET);
+			if (!in_queries[c-OFFSET]) res += num_chars(str, c);
+			cout << res << endl;
 		}
 	}
 	return 0;
